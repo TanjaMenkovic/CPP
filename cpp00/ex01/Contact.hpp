2733936d7938073c2e2a4d0e2cbe80c062c253df
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-05-28 15:26:36 +0300

    Update Contact.hpp

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -10,4 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONTACT_H
+# define CONTACT_H
 
+# include <string>
+# include <iostream>
+# include <iomanip>
+
+class Contact {
+	private:
+		int			_index;
+		std::string	_firstName;
+		std::string	_lastName;
+		std::string	_nickName;
+		std::string	_phoneNumber;
+		std::string	_darkestSecret;
+
+	public:
+		Contact();
+		~Contact();
+		
+		void		Contact::set_index(int index);
+		int			Contact::get_index(void) const;
+		std::string	Contact::get_firstname(void) const;
+		std::string	Contact::get_lastname(void) const;
+		std::string	Contact::get_nickname(void) const;
+		std::string	Contact::get_phonenumber(void) const;
+		std::string	Contact::get_darkestsecret(void) const;
+
+		void		init(int index);
+}
+
+#endif
