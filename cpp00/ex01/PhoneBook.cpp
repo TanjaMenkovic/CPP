b86e2cf2a4518cb1dff4161356489651e4219602
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-05-28 15:27:09 +0300

    Update PhoneBook.cpp

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
 
+/*
+default constructor
+*/
+PhoneBook::~PhoneBook()
+{
+	std::cout << "There is no contact in this phone book!" << std::endl;
+}
+
+/*
+constructor
+*/
+PhoneBook::PhoneBook()
+{
+	int	i = 0;
+
+	while (i < 8)
+	{
+		this->_contacts[i].set_index(-1);
+		i++;
+	}
+}
